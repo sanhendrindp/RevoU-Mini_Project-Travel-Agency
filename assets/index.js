9f4cbc962084824209a83b7116a64b0cbafe382d
sanhendrindp <sanhendrin.dwip@gmail.com>
2023-03-09 23:06:47 +0700

    update contact us

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Banner
 let slideShow = 1;
 setInterval(function () {
   document.getElementById('radio' + slideShow).checked = true;
@@ -8,3 +9,18 @@ setInterval(function () {
     slideShow = 1;
   }
 }, 5000);
+
+// Contact us
+const sendBtn = document.querySelector('.submit-send');
+
+sendBtn.addEventListener('click', function () {
+  const inputName = document.querySelector('.input-name').value;
+  const inputEmail = document.querySelector('.input-email').value;
+  const inputMessage = document.querySelector('.input-message').value;
+
+  if (!inputName || !inputEmail || !inputMessage) {
+    alert('Please complete your data');
+  } else {
+    alert('Your question has been sent');
+  }
+});
